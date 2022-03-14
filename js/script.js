ca54018767b0252a88cbef9d5c09f63718b2a437
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-03-14 19:54:38 +0300

    index.html: Added the portfolio section, script.js: Added the hover effects for the portfollio section style.css: Styled the portfolio section

--- js/script.js
+++ js/script.js
@@ -64,6 +64,85 @@ jQuery(document).ready(function() {
           $("#pproduct").hide();
           $("#product").animate({fontWeight:'300',fontSize:'20px'});
           });
+
+
+///Adding the hover effect
+        $('#work1').hover(function() {
+            $(this).animate({opacity:0.3},300);
+            $('.portdescription1').show(700);
+        },
+        function(){
+            $(this).animate({opacity:1},300);
+            $('.portdescription1').hide(300);
+        });
+
+        $('#work2').hover(function() {
+          $(this).animate({opacity:0.3},300);
+          $('.portdescription2').show(700);
+      },
+      function(){
+          $(this).animate({opacity:1},300);
+          $('.portdescription2').hide(300);
+      });
+
+      $('#work3').hover(function() {
+        $(this).animate({opacity:0.3},300);
+        $('.portdescription3').show(700);
+    },
+    function(){
+        $(this).animate({opacity:1},300);
+        $('.portdescription3').hide(300);
+    });
+
+
+        $('#work4').hover(function() {
+          $(this).animate({opacity:0.3},300);
+          $('.portdescription4').show(700);
+      },
+      function(){
+          $(this).animate({opacity:1},300);
+          $('.portdescription4').hide(300);
+      });
+
+
+      $('#work5').hover(function() {
+        $(this).animate({opacity:0.3},300);
+        $('.portdescription5').show(700);
+    },
+    function(){
+        $(this).animate({opacity:1},300);
+        $('.portdescription5').hide(300);
+    });
     
 
+        $('#work6').hover(function() {
+          $(this).animate({opacity:0.3},300);
+          $('.portdescription6').show(700);
+      },
+      function(){
+          $(this).animate({opacity:1},300);
+          $('.portdescription6').hide(300);
+      });
+
+
+        $('#work7').hover(function() {
+          $(this).animate({opacity:0.3},300);
+          $('.portdescription7').show(700);
+      },
+      function(){
+          $(this).animate({opacity:1},300);
+          $('.portdescription7').hide(300);
+      });
+
+
+      $('#work8').hover(function() {
+        $(this).animate({opacity:0.3},300);
+        $('.portdescription8').show(700);
+    },
+    function(){
+        $(this).animate({opacity:1},300);
+        $('.portdescription8').hide(300);
     });
+    });
+
+
